d6f700ec71ac283a639b806ff17b554e8fa0156b
Mariam Safa <88893709+mariamsafa@users.noreply.github.com>
2022-04-16 23:54:47 +0600

    Add files via upload

--- review/282A.c
+++ review/282A.c
@@ -1,1 +1,36 @@
+#include<stdio.h>
+int main()
+{
+   int i,n,x=0;
+   char ch[5];
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+       scanf("%s", ch);
+       if(ch[0]=='+'&&ch[1]=='+')
+       {
+           x++;
+
+       }
+       else if(ch[1]=='+'&&ch[2]=='+')
+       {
+           x++;
+
+       }
+       else if(ch[0]=='-'&&ch[1]=='-')
+       {
+           x--;
+
+
+       }
+       else if (ch[1]=='-'&&ch[2]=='-')
+       {
+           x--;
+       }
+   }
+    printf("%d\n",x);
+    return 0;
+}
+
+
 
