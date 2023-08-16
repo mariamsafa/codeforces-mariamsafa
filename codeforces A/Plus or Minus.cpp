f8c1ab022d28f09ec1b684c441acbcc9b3defc9c
Mariam Safa <88893709+mariamsafa@users.noreply.github.com>
2023-08-17 00:44:25 +0600

    Create Plus or Minus.cpp

---
+++ codeforces A/Plus or Minus.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,t;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b>>c;
+        if(a+b==c)
+        {
+            cout<<"+"<<endl;
+        }
+        else if(a-b==c)
+        {
+            cout<<"-"<<endl;
+        }
+    }
+}
